b7c51006897d4c948924969140b6dee3019ff338
AhmedJwad <Ahmednet380@gmail.com>
2023-12-04 08:21:05 -0800

    Adding Strings Array

---
+++ Strings/String.Ui.Consol/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strings.Logic;
+
+try
+{
+	Console.WriteLine("Hello, World!");
+	var mystring = new MyString("for");
+
+	Console.WriteLine(mystring.Ispalindrome());
+
+
+}
+catch (Exception ex)
+{
+
+	throw new Exception(ex.Message);
+}
---
+++ Strings/Strings.Logic/MyString.cs
@@ -0,0 +1,40 @@
+﻿namespace Strings.Logic
+{
+    public class MyString
+    {
+        private readonly string _text;
+
+        public MyString(String text)
+        {
+            _text = text;
+        }
+
+        public bool Ispalindrome()
+        {
+            string Aux2=_text.ToLower();
+            Aux2 = Aux2.Replace("ا", "a");
+            Aux2 = Aux2.Replace("ح", "h");
+            Aux2 = Aux2.Replace("م", "m");
+            Aux2 = Aux2.Replace("د", "d");
+            string Aux = string.Empty;
+            for (int i = 0; i < Aux2.Length; i++)
+            {
+                if (!(Aux2[i] == (' ') || Aux2[i] == ('.') || Aux2[i] == ('!') || Aux2[i] == ('?')
+                    || Aux2[i] == ('!') || Aux2[i] == '¡' || Aux2[i] == '!' || Aux2[i] == '\'' || Aux2[i] == '"'))
+                    {
+                    Aux += Aux2[i];
+                    }
+            }
+            int n = Aux.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                if (Aux[i] != Aux[Aux.Length-i-1])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
